e880f58519299302482098c3d5a3a6e93fedf8fd
Mansour <m_emami21@cs.uni-kl.de>
2025-08-15 12:33:09 +0200

    - Defines core methods for data structure operations:   - getRoot(): retrieves the root of the structure   - addItem(): adds a new ListItem   - removeItem(): removes a specified ListItem   - traverse(): iterates through the structure - Establishes a contract for linked lists and binary search trees.

--- src/info/emami/NodeList.java
+++ src/info/emami/NodeList.java
@@ -1,4 +1,11 @@
 package info.emami;
 
 public interface NodeList {
+
+    ListItem getRoot();
+    boolean addItem(ListItem newItem);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+
+
 }
