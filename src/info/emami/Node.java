81aa8143f9e72bab229c381f4fa3a24aec26e018
Mansour <m_emami21@cs.uni-kl.de>
2025-08-15 12:50:55 +0200

    comments for Node.java

--- src/info/emami/Node.java
+++ src/info/emami/Node.java
@@ -7,32 +7,32 @@ public class Node extends ListItem{
     }
 
     @Override
-    ListItem next() {
+    ListItem next() { // Returns the next node in the list/tree
         return this.rightLink;
     }
 
     @Override
-    ListItem setNext(ListItem item) {
+    ListItem setNext(ListItem item) { // Sets the next node in the list/tree
         this.rightLink = item;
         return this.rightLink;
     }
 
     @Override
-    ListItem previous() {
+    ListItem previous() { // Returns the previous node in the list/tree
         return this.leftLink;
     }
 
     @Override
-    ListItem setPrevious(ListItem item) {
+    ListItem setPrevious(ListItem item) { // Sets the previous node in the list/tree
         this.leftLink = item;
         return this.leftLink;
     }
 
     @Override
-    int compareTo(ListItem item) {
+    int compareTo(ListItem item) { // Compares this node's value with another node's value for sorting
 
         if(item != null){
-            return ((String) super.getValue()).compareTo((String) item.getValue());
+            return ((String) super.getValue()).compareTo((String) item.getValue()); // Assuming values are Strings for comparison
         }else{
             return -1;
         }
