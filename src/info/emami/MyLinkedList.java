e1b0a6759ebda7eb60793e8fad52571a08b393ec
Mansour <m_emami21@cs.uni-kl.de>
2025-08-15 13:08:37 +0200

    the first step of MyLinkedlist . implement NodeList and its methods

--- src/info/emami/MyLinkedList.java
+++ src/info/emami/MyLinkedList.java
@@ -1,4 +1,31 @@
 package info.emami;
 
-public class MyLinkedList {
+public class MyLinkedList implements NodeList {
+
+    private ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+
+    @Override
+    public ListItem getRoot() {
+        return null;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
 }
