73b82ede735611046f3fd06894e01d8123bde601
Mansour <m_emami21@cs.uni-kl.de>
2025-08-15 12:57:29 +0200

    comments for NodeList.java

--- src/info/emami/NodeList.java
+++ src/info/emami/NodeList.java
@@ -2,10 +2,10 @@ package info.emami;
 
 public interface NodeList {
 
-    ListItem getRoot();
-    boolean addItem(ListItem newItem);
-    boolean removeItem(ListItem item);
-    void traverse(ListItem root);
+    ListItem getRoot(); // Returns the root node of the list/tree
+    boolean addItem(ListItem newItem); // Adds a new item to the list/tree
+    boolean removeItem(ListItem item); // Removes an item from the list/tree
+    void traverse(ListItem root); // Traverses the list/tree starting from the given root node
 
 
 }
